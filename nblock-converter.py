4c18b552c6e92e96ac62a4d125be27d69190b004
Miftah Afina <miftah.afina@gmail.com>
2021-09-19 08:53:25 +0700

    Add an exception

--- nblock-converter.py
+++ nblock-converter.py
@@ -5,16 +5,18 @@ while True:
     if block == 'exit':
         break
     else:
-        block = int(block)
+        try:
+            block = int(block)
+            if block >= 8192:
+                result = block / 8192
+                unit   = 'GB'
+            elif block >= 8:
+                result = block / 8
+                unit   = 'MB'
+            else:
+                result = block * 128
+                unit   = 'KB'
+            print (f'=> {block} block = {result} {unit}')
 
-        if block >= 8192:
-            result = block / 8192
-            unit   = 'GB'
-        elif block >= 8:
-            result = block / 8
-            unit   = 'MB'
-        else:
-            result = block * 128
-            unit   = 'KB'
-
-    print (f'{block} block = {result} {unit}')
+        except ValueError:
+            print(f'=> {block} is not an integer number')
